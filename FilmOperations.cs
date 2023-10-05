1fd9c9ee15539009a3e08981d2c7a1d16bdb9dcb
Snipes09 <125398172+Snipes09@users.noreply.github.com>
2023-10-05 21:19:39 +0200

    Excercise finished

--- FilmOperations.cs
+++ FilmOperations.cs
@@ -1,35 +1,70 @@
-namespace LinqExercise;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
 
-public class FilmOperations
+namespace LinqExercise
+
 {
-    /// <returns>eine Liste aller Filme zurück.</returns>
-    public List<Film> GetAllMovies()
+    public class FilmOperations
     {
-        throw new NotImplementedException();
-    }
+      
+        private List<Film> _movies;
 
-    /// <returns>ein Array von Filmen zurück, die von dem angegebenen Regisseur stammen.</returns>
-    public Film[] GetMoviesByDirector(string directorName)
-    {
-        throw new NotImplementedException();
-    }
+        public FilmOperations()
+        {
+            _movies = GetAllMovies();
+        }
 
-    /// <returns>ein Array von Filmen zurück, die im angegebenen Erscheinungsjahr veröffentlicht wurden.</returns>
-    public Film[] GetMoviesByYear(int releaseYear)
-    {
-        throw new NotImplementedException();
-    }
-   
-    /// <returns>ein Array von Filmen zurück, die zwischen der angegebenen Mindest- und Höchstbewertung liegen.</returns>
-    public Film[] GetMoviesByRatingRange(double minRating, double maxRating)
-    {
-        throw new NotImplementedException();
-    }
-    
-    /// <returns>gibt ein Array mit den best bewerteten Filme zurück, sortiert nach Bewertung. numberOfFilms gibt die
-    /// Anzahl der zurückgegeben Filme an.</returns>
-    public Film[] GetMoviesByRatingSortedLimited(int numberOfFilms)
-    {
-        throw new NotImplementedException();
-    }
+
+        /// <returns>eine Liste aller Filme zurück.</returns>
+        public List<Film> GetAllMovies()
+        {
+            using (StreamReader reader = new StreamReader(@"C:\Users\Codersbay\aufgabe-7-linq-Snipes09\films.json"))
+            {
+                string json = reader.ReadToEnd();
+                _movies = JsonConvert.DeserializeObject<List<Film>>(json);
+            }
+            return _movies;
+        }
+
+         /// <returns>ein Array von Filmen zurück, die von dem angegebenen Regisseur stammen.</returns>
+         public Film[] GetMoviesByDirector(string director)
+         {
+              // return movies.Where(f => f.Director == director).ToArray();
+              var query = 
+                  from movie in _movies
+                  where movie.Director == director
+                  select movie;
+
+              return query.ToArray();
+         }
+
+         /// <returns>ein Array von Filmen zurück, die im angegebenen Erscheinungsjahr veröffentlicht wurden.</returns>
+         public Film[] GetMoviesByYear(int releaseYear)
+         {
+            // return movies.Where(f => f.ReleaseYear == releaseYear).ToArray();
+            var query = 
+                from movie in _movies
+                where movie.ReleaseYear == releaseYear
+                select movie;
+
+            return query.ToArray();
+         }
+
+         /// <returns>ein Array von Filmen zurück, die zwischen der angegebenen Mindest- und Höchstbewertung liegen.</returns>
+         public Film[] GetMoviesByRatingRange(double minRating, double maxRating)
+         {
+           return _movies.Where(f => f.Rating >= minRating && f.Rating <= maxRating).ToArray();
+         }
+
+
+         /// <returns>gibt ein Array mit den best bewerteten Filme zurück, sortiert nach Bewertung. numberOfFilms gibt die
+         /// Anzahl der zurückgegeben Filme an.</returns>
+         public Film[] GetMoviesByRatingSortedLimited(int numberOfFilms)
+         {
+             return _movies.OrderByDescending(f => f.Rating).Take(numberOfFilms).ToArray();
+         }
+   }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,50 @@
-﻿namespace LinqExercise;
+﻿using System;
 
-/*
- * your tests here
- */
+namespace LinqExercise
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            FilmOperations filmOperations = new FilmOperations();
+
+            var allMovies = filmOperations.GetAllMovies();
+            Console.WriteLine("All Movies:");
+            foreach (Film film in allMovies)
+            {
+                Console.WriteLine(
+                    $"{film.Title} | Regisseur: {film.Director} | Release Year: {film.ReleaseYear} | Rating: {film.Rating}");
+            }
+
+            Console.WriteLine("\nMovies from Francis Ford Coppola:");
+            var moviesByFrancisFordCoppola = filmOperations.GetMoviesByDirector("Francis Ford Coppola");
+            foreach (Film film in moviesByFrancisFordCoppola)
+            {
+                Console.WriteLine($"{film.Title} {film.Director} {film.ReleaseYear} {film.Rating}");
+            }
+
+            Console.WriteLine("\nMovies from 1994:");
+            var filmsFrom1994 = filmOperations.GetMoviesByYear(1994);
+            foreach (Film film in filmsFrom1994)
+            {
+                Console.WriteLine($"{film.Title} {film.Director} {film.ReleaseYear} {film.Rating}");
+            }
+
+            Console.WriteLine("\nMovies with rating between 8 and 10:");
+            var filmsByRating = filmOperations.GetMoviesByRatingRange(8.0, 10.0);
+            foreach (Film film in filmsByRating)
+            {
+                Console.WriteLine($"{film.Title} {film.Director} {film.ReleaseYear} {film.Rating}");
+
+            }
+
+            Console.WriteLine("\nThe 5 best rating movies:");
+            var top5RatedFilms = filmOperations.GetMoviesByRatingSortedLimited(5);
+            foreach (Film film in top5RatedFilms)
+            {
+                Console.WriteLine($"{film.Title} {film.Director} {film.ReleaseYear} {film.Rating}");
+            }
+
+        }
+    }
+}   
